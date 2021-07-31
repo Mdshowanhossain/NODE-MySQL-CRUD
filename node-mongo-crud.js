e2088a6ddfc950649b64819f9761558b450ccdb3
Mdshowanhossain <76901439+Mdshowanhossain@users.noreply.github.com>
2021-07-31 15:17:16 +0600

    NODE-MySQL-CRUD

--- node-mongo-crud.js
+++ node-mongo-crud.js
@@ -1,37 +1,85 @@
-// node-mongo
-// node-mongo123456
+const mysql = require('mysql');
 
+const DatabaseConfig = {
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'crue'
+}
 
-const MongoClient = require('mongodb').MongoClient;
+const connection = mysql.createConnection(DatabaseConfig);
 
-const URL = "mongodb+srv://node-mongo:node-mongo123456@cluster0.buk2o.mongodb.net/?retryWrites=true&w=majority"
-
-MongoClient.connect(URL, (error, MongoClientConnection) => {
-
+connection.connect((error) => {
     if (error) {
-        console.log('Your Connection is Failed');
+        console.log('Connection Failed');
     } else {
-        console.log('Your Connection is Success');
-
-        insertData(MongoClientConnection);
+        console.log('Connection Successful');
+        // insertData(connection);
+        // deleteData(connection);
+        // dataUpdate(connection);
+        selectData(connection);
     }
 })
 
+const insertData = () => {
 
-const insertData = (MongoClientConnection) => {
+    let SQL_Query = "INSERT INTO `students`(`Name`, `Father Name`, `Mother Name`, `Phone`, `Roll`) VALUES ('HERO','ABDUL HAY','RASHIDA BEGUM','01998110072','03')"
 
-    const MyDataBaseConnection = MongoClientConnection.db("School");
+    connection.query(SQL_Query, (error) => {
 
-    const MyCollection = MyDataBaseConnection.collection("Students");
-
-    // var myData = { "name": "SOHAN", "Roll": "01", "Class": "ten", "City": "Khulna" }
-    // const myData = { name: "OSMAN", age: "22", Roll: "02", Class: "Nini" }
-
-    MyCollection.insertOne(myData, (error) => {
         if (error) {
             console.log('Data Inserted Failed');
+
         } else {
             console.log('Data Inserted Success');
+        }
+    })
+}
+
+const deleteData = (connection) => {
+
+    let SQL_Query = "DELETE FROM `students` WHERE `id`= '03'";
+
+    connection.query(SQL_Query, (error) => {
+
+        if (error) {
+            console.log('Data Delete Failed');
+
+        } else {
+            console.log('Data Delete Success');
+        }
+    })
+}
+
+const dataUpdate = (connection) => {
+
+    let SQL_Query = "UPDATE `students` SET `Name`='RUMI',`Father Name`='ANISUR RAHMAN',`Mother Name`='CHINA BEGUM',`Roll`='44' WHERE `id`='1'";
+
+    connection.query(SQL_Query, (error) => {
+
+        if (error) {
+            console.log(error);
+            console.log('Data Updated Failed');
+
+        } else {
+            console.log('Data Updated Success');
+        }
+    })
+}
+
+
+const selectData = (connection) => {
+
+    let SQL_Query = "SELECT * FROM `students` WHERE 1"
+
+    connection.query(SQL_Query, (error, result) => {
+
+        if (error) {
+            console.log(error);
+            console.log(error);
+
+        } else {
+            console.log(result);
         }
     })
 }
